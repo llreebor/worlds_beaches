7df4db3c139864b90dadb0b91f863969894f267a
mac <mac@Roman-Timoshchuk.local>
2024-04-04 15:31:55 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,41 @@
+// Menu
+function burgerMenu() {
+	const burger = document.querySelector('.burger')
+	const menu = document.querySelector('.menu')
+	const body = document.querySelector('body')
+	burger.addEventListener('click', () => {
+		if (!menu.classList.contains('active')) {
+			menu.classList.add('active')
+			burger.classList.add('active')
+			body.classList.add('locked')
+		} else {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('locked')
+		}
+	})
+	// Вот тут мы ставим брейкпоинт навбара
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > 991.98) {
+			menu.classList.remove('active')
+			burger.classList.remove('active')
+			body.classList.remove('locked')
+		}
+	})
+}
+burgerMenu()
+function fixedHeader() {
+	const header = document.querySelector('header')
+	const breakpoint = 40
+	if (window.scrollY >= breakpoint) {
+		header.classList.add('active')
+	} else {
+		header.classList.remove('active')
+	}
+}
+window.addEventListener('scroll', fixedHeader)
+
+// Ambassador Slider
 const swiper = new Swiper('.swiper__ambassadors', {
 	loop: true,
 	centeredSlides: true,
@@ -22,3 +60,92 @@ const swiper = new Swiper('.swiper__ambassadors', {
 		},
 	},
 })
+
+// Accordion
+function accordion() {
+	const items = document.querySelectorAll('.accordion__item-trigger')
+	const contents = document.querySelectorAll('.accordion__item-content')
+	if (window.innerWidth < 600) {
+		items.forEach((item, idx) => {
+			item.addEventListener('click', () => {
+				const parent = item.parentNode
+				if (parent.classList.contains('accordion__item-active')) {
+					parent.classList.remove('accordion__item-active')
+				} else {
+					slideToggle(contents[idx])
+				}
+			})
+		})
+	}
+}
+accordion()
+/* SLIDE UP */
+let slideUp = (target, duration = 300) => {
+	target.style.transitionProperty = 'height, margin, padding'
+	target.style.transitionDuration = duration + 'ms'
+	target.style.boxSizing = 'border-box'
+	target.style.height = target.offsetHeight + 'px'
+	target.offsetHeight
+	target.style.overflow = 'hidden'
+	target.style.height = 0
+	target.style.paddingTop = 0
+	target.style.paddingBottom = 0
+	target.style.marginTop = 0
+	target.style.marginBottom = 0
+	target.style.border = 'none'
+
+	window.setTimeout(() => {
+		target.style.display = 'none'
+		target.style.removeProperty('height')
+		target.style.removeProperty('padding-top')
+		target.style.removeProperty('padding-bottom')
+		target.style.removeProperty('margin-top')
+		target.style.removeProperty('margin-bottom')
+		target.style.removeProperty('overflow')
+		target.style.removeProperty('transition-duration')
+		target.style.removeProperty('transition-property')
+		target.style.removeProperty('border')
+	}, duration)
+}
+/* SLIDE DOWN */
+let slideDown = (target, duration = 300) => {
+	target.style.removeProperty('display')
+	let display = window.getComputedStyle(target).display
+	if (display === 'none') display = 'grid'
+	target.style.display = display
+	let height = target.offsetHeight
+	target.style.overflow = 'hidden'
+	target.style.height = 0
+	target.style.paddingTop = 0
+	target.style.paddingBottom = 0
+	target.style.marginTop = 0
+	target.style.marginBottom = 0
+	target.offsetHeight
+	target.style.boxSizing = 'border-box'
+	target.style.transitionProperty = 'height, margin, padding'
+	target.style.transitionDuration = duration + 'ms'
+	target.style.height = height + 'px'
+	target.style.border = 'none'
+
+	target.style.removeProperty('padding-top')
+	target.style.removeProperty('padding-bottom')
+	target.style.removeProperty('margin-top')
+	target.style.removeProperty('margin-bottom')
+	target.style.removeProperty('border')
+
+	window.setTimeout(() => {
+		target.style.removeProperty('height')
+		target.style.removeProperty('overflow')
+		target.style.removeProperty('transition-duration')
+		target.style.removeProperty('transition-property')
+		target.style.removeProperty('border')
+	}, duration)
+}
+/* TOOGLE */
+const slideToggle = (target, duration = 300) => {
+	if (window.getComputedStyle(target).display === 'none') {
+		return slideDown(target, duration)
+	} else {
+		return slideUp(target, duration)
+	}
+}
