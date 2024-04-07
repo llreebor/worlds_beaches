6bba75769703cf134777d7d3453597c7140ef731
mac <mac@Roman-Timoshchuk.local>
2024-04-07 17:13:46 +0300

    fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -241,3 +241,114 @@ const slideToggle = (target, duration = 300) => {
 		return slideUp(target, duration)
 	}
 }
+
+// Tabs
+// function tabs(
+// 	headerSelector,
+// 	tabSelector,
+// 	contentSelector,
+// 	activeClass,
+// 	display = 'flex'
+// ) {
+// 	const header = document.querySelector(headerSelector),
+// 		tab = document.querySelectorAll(tabSelector),
+// 		content = document.querySelectorAll(contentSelector)
+
+// 	const nextBtn = document.querySelector('.tabs__header-item-next')
+
+// 	function hideTabContent() {
+// 		content.forEach((item) => {
+// 			item.style.display = 'none'
+// 		})
+// 		tab.forEach((item) => {
+// 			item.classList.remove(activeClass)
+// 		})
+// 	}
+// 	function showTabContent(i = 0) {
+// 		content[i].style.display = display
+// 		tab[i].classList.add(activeClass)
+// 	}
+// 	hideTabContent()
+// 	showTabContent()
+// 	header.addEventListener('click', (e) => {
+// 		const target = e.target
+// 		if (
+// 			target.classList.contains(tabSelector.replace(/\./, '')) ||
+// 			target.parentNode.classList.contains(tabSelector.replace(/\./, ''))
+// 		) {
+// 			tab.forEach((item, i) => {
+// 				if (target == item || target.parentNode == item) {
+// 					hideTabContent()
+// 					showTabContent(i)
+// 				}
+// 			})
+// 		}
+// 	})
+// }
+
+// ПЕРВЫЙ аргумент - класс всего нашего хедера табов.
+// ВТОРОЙ аргумент - класс конкретного элемента, при клике на который будет переключатся таб.
+// ТРЕТИЙ аргумент - класс того блока, который будет переключаться.
+// ЧЕТВЕРТЫЙ аргумент - класс активности, который будет добавлятся для таба, который сейчас активен.
+// tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
+function tabs(
+	headerSelector,
+	tabSelector,
+	contentSelector,
+	activeClass,
+	display = 'flex'
+) {
+	const header = document.querySelector(headerSelector),
+		tab = document.querySelectorAll(tabSelector),
+		content = document.querySelectorAll(contentSelector)
+
+	const nextBtn = document.querySelector('.tabs__header-item-next')
+
+	function hideTabContent() {
+		content.forEach((item) => {
+			item.style.display = 'none'
+		})
+		tab.forEach((item) => {
+			item.classList.remove(activeClass)
+		})
+	}
+
+	function showTabContent(i = 0) {
+		content[i].style.display = display
+		tab[i].classList.add(activeClass)
+	}
+
+	function showNextTab() {
+		const currentActiveIndex = Array.from(tab).findIndex((item) =>
+			item.classList.contains(activeClass)
+		)
+		if (currentActiveIndex < tab.length - 1) {
+			hideTabContent()
+			showTabContent(currentActiveIndex + 1)
+		}
+	}
+
+	hideTabContent()
+	showTabContent()
+
+	header.addEventListener('click', (e) => {
+		const target = e.target
+		if (
+			target.classList.contains(tabSelector.replace(/\./, '')) ||
+			target.parentNode.classList.contains(tabSelector.replace(/\./, ''))
+		) {
+			tab.forEach((item, i) => {
+				if (target == item || target.parentNode == item) {
+					hideTabContent()
+					showTabContent(i)
+				}
+			})
+		}
+	})
+
+	nextBtn.addEventListener('click', () => {
+		showNextTab()
+	})
+}
+
+tabs('.tabs__header', '.tabs__header-item', '.tabs__content-item', 'active')
